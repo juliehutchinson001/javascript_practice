2bae8dbc45982481389230e97cc3efb6e9bcf701
Julie Hutchinson <julie.hutchinson001@outlook.com>
2017-12-30 23:12:24 -0500

    feat: access data through an API

---
+++ js_practice_dec_30/app.js
@@ -0,0 +1,31 @@
+//Require https module
+const https = require('https');
+
+//Function to print message to console
+function printMessage(username, badgeCount, points) {
+    const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
+    console.log(message);
+}
+
+function getProfile(username) {
+    // Connect to the API URL (https://teamtreehouse.com/username.json)
+    const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+        let body = "";
+        // Read the data
+        response.on('data', data => {
+            body += data.toString();
+        });
+
+        response.on('end', () => {
+            // Parse the data
+            const profile = JSON.parse(body);
+            // Print the data
+            printMessage(username, profile.badges.length, profile.points.JavaScript);
+        });
+
+    });
+
+}
+
+const users = process.argv.slice(2);
+users.forEach(getProfile);
