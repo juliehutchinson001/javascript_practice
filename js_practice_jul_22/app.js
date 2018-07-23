bba00347c4e6851c62ebeaa0e04a8d3d6d29714d
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-07-22 21:25:57 -0700

    init: sample problem instructions

---
+++ js_practice_jul_22/app.js
@@ -0,0 +1,23 @@
+// 3) Write a function that looks at a string and 
+// scrambles the letters between the first and last. 
+// This function should take in a String as a parameter, 
+// and return a String. The first letter and the last 
+// letter of the param String should stay, the letters 
+// between the first and last will be shuffled/randomized/scrambled. 
+
+// 2) Write a function that takes a string splits 
+// the words into an array of strings and scrambles 
+// each word using the function above. Then scramble the quote.
+
+
+
+
+
+
+
+
+
+
+
+
+
