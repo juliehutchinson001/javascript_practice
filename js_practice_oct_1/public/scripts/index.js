902a39ba58132293910f63873a51824e2d7bd245
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-10-04 14:08:52 -0700

    feat: creating methods to update my state

--- js_practice_oct_1/public/scripts/index.js
+++ js_practice_oct_1/public/scripts/index.js
@@ -1,42 +1,136 @@
 "use strict";
 
-var toggleMessage = false;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var app = {
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+/*
+
+let toggleMessage = false;
+
+const app = {
     name: "Visibility App"
 };
 
-var toggle = function toggle() {
+const toggle = () => {
 
-    toggleMessage = !toggleMessage;
+    toggleMessage = !toggleMessage; 
     render();
+
+}
+
+const appRoot = document.getElementById('app');
+
+const render = () => {
+
+    const sample = (
+
+        <div>
+            <h1>{ app.name }</h1>
+            <button onClick={ toggle } >
+            { toggleMessage ? "Hide Details" : "Show Details" }
+            </button>
+            {toggleMessage && (<p>This details can be seen now</p>)}
+        </div>    
+
+    );
+
+    ReactDOM.render(sample, appRoot);
+
 };
 
-var appRoot = document.getElementById('app');
+render();
 
-var render = function render() {
+*/
 
-    var sample = React.createElement(
+/* ***** The New Way ***** */
+
+var VisibilityContainer = function (_React$Component) {
+    _inherits(VisibilityContainer, _React$Component);
+
+    function VisibilityContainer(props) {
+        _classCallCheck(this, VisibilityContainer);
+
+        var _this = _possibleConstructorReturn(this, (VisibilityContainer.__proto__ || Object.getPrototypeOf(VisibilityContainer)).call(this, props));
+
+        _this.state = {
+            detailsText: '',
+            buttonText: "Show Details"
+        };
+
+        return _this;
+    }
+
+    _createClass(VisibilityContainer, [{
+        key: "getDetailsText",
+        value: function getDetailsText() {
+
+            var text = !this.state.detailsText ? "these details ..." : "";
+            return text;
+        }
+    }, {
+        key: "getButtonText",
+        value: function getButtonText() {
+
+            var buttonText = this.state.buttonText === "Show Details" ? "Hide Details" : "Show Details";
+            return buttonText;
+        }
+    }, {
+        key: "handleClick",
+        value: function handleClick() {
+
+            this.setState({
+                detailsText: this.getDetailsText(),
+                buttonText: this.getButtonText()
+            });
+        }
+    }, {
+        key: "render",
+        value: function render() {
+            var _this2 = this;
+
+            return React.createElement(Visibility, {
+                detailsText: this.state.detailsText,
+                buttonText: this.state.buttonText,
+                clickHandler: function clickHandler() {
+                    return _this2.handleClick();
+                }
+            });
+        }
+    }]);
+
+    return VisibilityContainer;
+}(React.Component);
+
+;
+
+var Visibility = function Visibility(_ref) {
+    var detailsText = _ref.detailsText,
+        buttonText = _ref.buttonText,
+        clickHandler = _ref.clickHandler;
+    return React.createElement(
         "div",
         null,
         React.createElement(
             "h1",
             null,
-            app.name
+            "Visibility App"
+        ),
+        React.createElement(
+            "p",
+            null,
+            detailsText
         ),
         React.createElement(
             "button",
-            { onClick: toggle },
-            toggleMessage ? "Hide Details" : "Show Details"
-        ),
-        toggleMessage && React.createElement(
-            "p",
-            null,
-            "This details can be seen now"
+            { onClick: clickHandler },
+            buttonText
         )
     );
-
-    ReactDOM.render(sample, appRoot);
 };
 
-render();
+ReactDOM.render(React.createElement(VisibilityContainer, null), document.getElementById('app'));
