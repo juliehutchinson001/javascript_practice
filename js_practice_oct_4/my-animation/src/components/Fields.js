f741ef766bd85f8fd0057ef0618d9c84a82ab3c9
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-10-09 20:40:12 -0700

    refactor: tyde up names and UI structure

--- js_practice_oct_4/my-animation/src/components/Fields.js
+++ js_practice_oct_4/my-animation/src/components/Fields.js
@@ -1,38 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class FieldsContainer extends Component {
-    
-    constructor(props) {
-
-        super(props);
-    }
-    
-    render() {
-
-        return (
-            <Fields 
-                email={ email } 
-                onChange={onChange} 
-                onSubmit={ onSubmit } 
-                password={ password }
-                user={ user }
-            />
-        );
-
-    }
-};
-
-const Fields = ( {email, onChange, onSubmit, password, user} ) => {
+const Fields = ( {email, handleChange, handleSubmit, password, user} ) => {
     return (
         <form>
             <label htmlFor='user-name' >Name: 
-                <input id='user-name' type='text' name='name' />
+                <input id='user-name' type='text' name='name' onChange={ event => handleChange(event) } />
             </label>
             <label htmlFor='user-email' >Email: 
-                <input id='user-email' type='email' name='email' />
+                <input id='user-email' type='email' name='email' onChange={ event => handleChange(event) } />
             </label>
             <label htmlFor='user-password' >Password: 
-                <input id='user-password' type='password' name='password' />
+                <input id='user-password' type='password' name='password' onChange={ event => handleChange(event) } />
             </label>
             <input type='submit' value='submit' />
         </form>
