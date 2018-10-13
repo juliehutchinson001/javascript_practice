40a3bd71a89921d629ebf25b8de68245070ef030
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-10-12 22:31:04 -0700

    code: this sample authenticates validity of name, email and password of form

--- js_practice_oct_4/my-animation/src/helpers/check_errors.js
+++ js_practice_oct_4/my-animation/src/helpers/check_errors.js
@@ -18,4 +18,54 @@
 //     errorId: event.target.id
 // }]
 
+const checkName = name => name.length > 1;
 
+const checkEmail = email => email.includes('.com');
+
+const checkPassword = password => password.length > 6;
+
+const checkErrors = fields => {
+    debugger;
+    const fieldsNames = Object.keys(fields);
+    
+    const errors = fieldsNames.map(fieldName => {
+        switch(fieldName) {
+            case 'name':
+                const nameFieldHasError = !checkName(fields[fieldName].value);
+
+                if (nameFieldHasError) {
+                    return {
+                        errorName: 'Please type in a valid name',
+                        errorId: fields[fieldName].id
+                    };
+                }
+                break; 
+
+            case 'email':
+                const emailFieldIsWrong = !checkEmail(fields[fieldName].value);
+                
+                if (emailFieldIsWrong) {
+                    return {
+                        errorName: 'Please type in a valid email',
+                        errorId: fields[fieldName].id
+                    };
+                }
+                break; 
+
+            case 'password':
+                const passwordFieldIsWrong = !checkPassword(fields[fieldName].value);
+
+                if (passwordFieldIsWrong) {
+                    return {
+                        errorName: 'Please type in a valid password',
+                        errorId: fields[fieldName].id
+                    };
+                }
+                break; 
+        }
+    });
+
+    return errors;
+};
+
+export default checkErrors;
