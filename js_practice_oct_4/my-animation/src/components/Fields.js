d74dab896f4aa888697c77ce10eced5e50923c44
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-10-12 23:38:29 -0700

    refactor: change to a functional component of the form

--- js_practice_oct_4/my-animation/src/components/Fields.js
+++ js_practice_oct_4/my-animation/src/components/Fields.js
@@ -1,20 +1,12 @@
 import React from 'react';
 
-const Fields = ( {email, handleChange, handleSubmit, password, user} ) => {
-    return (
-        <form>
-            <label htmlFor='user-name' >Name: 
-                <input id='user-name' type='text' name='name' onChange={ event => handleChange(event) } />
+const Fields = ({ email, handleChange, handleSubmit, password, user }) => (
+        <form onSubmit={ handleSubmit }>
+            <label>Name:
             </label>
-            <label htmlFor='user-email' >Email: 
-                <input id='user-email' type='email' name='email' onChange={ event => handleChange(event) } />
-            </label>
-            <label htmlFor='user-password' >Password: 
-                <input id='user-password' type='password' name='password' onChange={ event => handleChange(event) } />
-            </label>
+
             <input type='submit' value='submit' />
         </form>
-    );
-};
+);
 
-export default FieldsContainer;
+export default Fields;
