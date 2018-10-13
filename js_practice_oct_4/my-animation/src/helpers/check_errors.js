18245179c0c531723df71b55227ff95262dd4582
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-10-12 22:13:45 -0700

    init: begin helper function to expand functionality of form

---
+++ js_practice_oct_4/my-animation/src/helpers/check_errors.js
@@ -0,0 +1,21 @@
+// formFields: {
+//     name: {
+//         value: '',
+//         id: 'user-name'
+//     },
+//     email: {
+//         value: '',
+//         id: 'user-email'
+//     },
+//     password: {
+//         value: '',
+//         id: 'user-password'
+//     },
+// },
+
+// [{
+//     errorName: 'Please set a valid ' + event.target.name,
+//     errorId: event.target.id
+// }]
+
+
