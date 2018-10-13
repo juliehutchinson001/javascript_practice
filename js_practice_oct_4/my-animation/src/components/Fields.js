f054cd589343345444240727ef101fd6bc3d5f96
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-10-12 23:39:14 -0700

    feat: update state of email, name and password inputs

--- js_practice_oct_4/my-animation/src/components/Fields.js
+++ js_practice_oct_4/my-animation/src/components/Fields.js
@@ -4,7 +4,12 @@ const Fields = ({ email, handleChange, handleSubmit, password, user }) => (
         <form onSubmit={ handleSubmit }>
             <label>Name:
             </label>
-
+            <label>Email:
+                <input id='user-email' type='email' name='email' onChange={ handleChange } value={ email } />
+            </label>
+            <label>Password:
+                <input id='user-password' type='password' name='password' onChange={ handleChange } value={ password } />
+            </label>
             <input type='submit' value='submit' />
         </form>
 );
