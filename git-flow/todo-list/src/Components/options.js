301708460fa4df084ef4135d532af656f59c7269
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-11-14 14:18:30 -0800

    feat: add more options to each todo

--- git-flow/todo-list/src/Components/options.js
+++ git-flow/todo-list/src/Components/options.js
@@ -3,14 +3,17 @@ import React from 'react';
 const Options = ({ items, addItemToList }) => {
 
     const itemsLis = items.map((item, index) => (
-        <li key={ index }>
+        <li className='options__todo' key={ index }>
             <span>{ item }</span>
-            <select onChange={ event => addItemToList(item, event.target.value) } >
-                <option value='general'>General</option>
-                <option value='list1'>List1</option>
-                <option value='list2'>List2</option>
-                <option value='list3'>List1</option>
-                <option value='list4'>List2</option>
+            <select
+                className='options__container'
+                onChange={ event => addItemToList(item, event.target.value) }
+            >
+                <option value='General'>General</option>
+                <option value='InProgress'>In-Progress</option>
+                <option value='Urgent'>Urgent</option>
+                <option value='Pasts'>Pasts</option>
+                <option value='NonUrgent'>Non-Urgent</option>
             </select>
         </li>
     ));
