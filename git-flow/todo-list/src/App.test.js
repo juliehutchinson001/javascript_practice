7ba4fe57830600eca8f86c82b4b612674df453d6
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-11-17 20:08:42 -0800

    refactor: move file to testing folder'

--- git-flow/todo-list/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import App from './App';
-
-it('renders the text -Enter Item- to show', () => {
-  const { getByText } = render(<App />);
-  expect(getByText('Enter Item')).toBeInTheDOM();
-});
-
-test('')
