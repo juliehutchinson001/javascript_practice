6b1171473fff6efd51a36fe8e61c926f5c028491
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-12-24 01:53:37 -0500

    init: reverse vowels of a string in python

---
+++ js_practice_dec_24/reverse_string.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def reverse_string(string_to_be_reversed):
+
+
+def main():
+
+    result = reverse_string(string_to_be_reversed)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
