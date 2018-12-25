9252bdc8f517b2de149b850eae19463c2cca48ff
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-12-25 05:04:40 -0500

    feat: coding solution to reverse vowels in a string

--- js_practice_dec_24/reverse_string.py
+++ js_practice_dec_24/reverse_string.py
@@ -2,10 +2,24 @@ import sys
 
 
 def reverse_string(string_to_be_reversed):
+    # vowels_filtered
+    vowels = ''.join(
+        filter(lambda character: character in 'aeiuo', string_to_be_reversed))
+    reversed_result = ''
+    filtered_vowels_index = -1  # -2
+
+    for character in string_to_be_reversed:
+        if character in vowels:
+            reversed_result += vowels[filtered_vowels_index]
+            filtered_vowels_index -= 1
+        else:
+            reversed_result += character
+
+    return reversed_result
 
 
 def main():
-
+    string_to_be_reversed = sys.argv[1]
     result = reverse_string(string_to_be_reversed)
     print(result)
 
