7608ba7fa2410e0dadeb5fec5713a5bd5aa33384
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-07-29 16:21:53 -0700

    [feat] document functions, create a load spinner and refactor main to only call functionality of task manager

--- API/asana_take_home_Julie_Hutchinson/src/js/index.js
+++ API/asana_take_home_Julie_Hutchinson/src/js/index.js
@@ -1,58 +1,26 @@
-const app = new App();
-
-const insertTask = existingTasks => {
-  const taskTxt = document.querySelectorAll(".task-list__txt");
-
-  [...taskTxt].forEach(index => {
-    let currentElement = taskTxt[index];
-    console.log(taskTxt[index]);
-    showDeleteBtn(currentElement);
-
-    if (index < existingTasks.data.length) {
-      let existingTask = existingTasks.data[index].name;
-      currentElement.value = existingTask;
-    }
-  });
+var lastActiveTaskId;
+/**
+ * @summary Adds a spinner while app is fetching the tasks from Asana
+ * @returns void
+ */
+const addLoadingSpinner = () => {
+  const spinner = document.createElement("div");
+  spinner.classList.add("loading-spinner");
+  document.querySelector(".project-body").appendChild(spinner);
 };
 
-const main = () => {
-  // Sets up the initial state of the app
+/**
+ * @summary Main entry point of the app
+ * @returns void
+ */
+const main = async () => {
+  addInitialEventListeners();
+  addLoadingSpinner();
 
-  // Getting Dom elements
-  const showBtn = document.querySelector(".btn--secondary");
-  const hideBtn = document.querySelectorAll(".btn--inline");
-  const addTaskBtn = document.querySelector(".btn--primary");
+  const { data } = await getTasksFromAsana();
+  insertTasksToDom(data);
 
-  const taskList = document.querySelector(".task-list");
-
-  // Adding event listeners
-  hideBtn.forEach(addHideRowEvent);
-  showBtn.addEventListener("click", showAllRows);
-
-  addTaskBtn.addEventListener("click", () => {
-    // Create new task elements
-    let taskRow = document.createElement("div");
-    let taskTxt = document.createElement("input");
-    let taskHideBtn = document.createElement("button");
-    taskHideBtn.innerHTML = "Hide";
-
-    // Add event listeners
-    addHideRowEvent(taskHideBtn);
-    showDeleteBtn(taskTxt);
-
-    // Add classes
-    taskRow.classList.add("task-list__row");
-    taskTxt.classList.add("task-list__txt");
-    taskHideBtn.classList.add("btn", "btn--inline");
-
-    // Insert into the DOM
-    taskRow.appendChild(taskTxt);
-    taskRow.appendChild(taskHideBtn);
-    taskList.appendChild(taskRow);
-  });
-
-  // Getting any pre-existing tasks
-  getAllTasks();
+  document.querySelector(".loading-spinner").remove();
 };
 
-// main();
+main();
